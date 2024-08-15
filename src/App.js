4cd014be332c12d836cc7b1f9fe7cd9bd0c0918a
ralascanog <ralascanog@gmail.com>
2024-08-15 15:34:45 -0500

    adding cloudword charts

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import ChartComponent from "./components/ChartComponent";
+import CloudWord from "./components/CloudWord";
+import CloudWord2 from "./components/CloudWord2";
 
 function App() {
   return (
     <div className="App">
-      <ChartComponent/>
+      {/*<ChartComponent/>*/}
+      {/*<CloudWord/>*/}
+        <CloudWord2/>
     </div>
   );
 }
